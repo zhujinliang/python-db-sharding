df678b31f523412f71f749019d83bada2ce8be22
jlzhu <jl_zhu@foxmail.com>
2016-09-05 17:14:08 +0800

    [REV] Remove develop config from git version

--- core/configs/default.py
+++ core/configs/default.py
@@ -13,10 +13,10 @@ class Config(object):
     TEMPLATE_FOLDER = PROJECT_PATH + 'testapp/templates'
     STATIC_FOLDER = PROJECT_PATH + 'testapp/static'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://dev_w:6nvjq0_HW@192.168.1.250:3307/dada'
+    SQLALCHEMY_DATABASE_URI = 'mysql://test:test@192.168.1.250:3306/test'
 
     SQLALCHEMY_BINDS = {
-        'coupon_db': 'mysql://dev_w:6nvjq0_HW@192.168.1.250:3307/coupon_service_db',
+        'coupon_db': 'mysql://test:test@192.168.1.250:3306/test',
     }
 
     SHARDING_NUM = 10
